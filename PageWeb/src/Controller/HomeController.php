e6c13938f6a807781d6c80a8ce0a05306f3f423e
Damien <damien@philippe.terraillon@gmail.com>
2022-04-19 09:37:22 +0200

    Initial commit

---
+++ PageWeb/src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @var Environment
+     */
+     
+    private $twig;
+    
+        public function __construct($twig)
+    {
+        $this->twig =  $twig;
+    }
+    
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return new Response($this->twig->render('pages/home.html.twig'));
+    }
+    
+}
